0692d4e0c2c9abfca5e52d16102c85099861a357
JACE <jeysonm021@gmail.com>
2023-01-25 21:34:33 -0500

    filtering

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,9 +1,29 @@
 // import { products, categories } from "../db.js";
 
 export const Query = {
-  products: (parent, { filter }, { products }) => {
-    if (!filter) return products;
-    return products.filter((product) => product.onSale === filter.onSale);
+  products: (parent, { filter }, { products, reviews }) => {
+    const { onSale, aveRating } = filter || {};
+    // console.log(filter);
+    // console.log(products);
+    if (onSale) return products.filter((product) => product.onSale === onSale);
+    if (aveRating) {
+      console.log(reviews);
+      const filteredProducts = products.filter((product) => {
+        let sumRating = 0;
+        let numReviews = 0;
+        reviews.forEach((review) => {
+          if (review.productId === product.id) {
+            sumRating += review.rating;
+            numReviews++;
+          }
+        });
+        console.log(sumRating, product.name);
+        console.log(numReviews);
+        return sumRating / numReviews >= aveRating;
+      });
+      return filteredProducts;
+    }
+    return products;
   },
   product: (parent, { id }, { products }) =>
     products.find((product) => product.id === id),
--- schema.js
+++ schema.js
@@ -40,5 +40,6 @@ export const typeDefs = `#graphql
 
   input ProductsFilterInput{
     onSale: Boolean
+    aveRating: Int
   }
 `;
